a28d0c8c98f18632c62479ac9e87c94240680ac0
szander <szander@lin-magdeburg.de>
2026-01-09 15:20:43 +0100

    feat(security): remove unused library imports and debug print in PasskeysWeb stub

--- client/local_packages/passkeys_web/lib/passkeys_web.dart
+++ client/local_packages/passkeys_web/lib/passkeys_web.dart
@@ -1,8 +1,5 @@
 // Web stub for passkeys package to prevent crashes on web
 // This allows the passkeys package to work on mobile while web uses custom JavaScript
-library passkeys_web;
-
-import 'package:flutter/foundation.dart';
 
 /// Stub web implementation of PasskeysWeb that doesn't crash
 /// Web platform uses custom JavaScript WebAuthn (see web/index.html)
@@ -11,9 +8,6 @@ class PasskeysWeb {
   static void registerWith(dynamic registrar) {
     // Empty implementation - prevents null reference crash
     // Web uses custom JavaScript WebAuthn implementation
-    debugPrint(
-      '[PasskeysWeb] Stub registered - using custom JavaScript WebAuthn',
-    );
   }
 
   /// Dummy constructor
